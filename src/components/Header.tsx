0c6766e8968cda0eadab1a4657d2c53233e2d3f2
Brunhilde <brunhildevink1995@gmail.com>
2020-11-04 19:45:22 +0100

    forgot header font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,8 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   headerTitle: {
     marginTop: "100px",
-    fontSize: "1.6em"
+    fontSize: "1.6em",
+    fontFamily: "roboto, sans serif"
   }
 }));
 
