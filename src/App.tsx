3250ea0dd0f96f01002c21ee58e0b9e2021c0d8e
Brunhilde <brunhildevink1995@gmail.com>
2020-11-04 12:05:08 +0100

    implemented api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 //models
 import { PostModel } from './models/PostModel';
@@ -7,26 +7,36 @@ import { PostModel } from './models/PostModel';
 import Post from './components/Post';
 
 function App() {
-  const [data, setData] = useState<PostModel[]>([
-    {
-      userId: 1,
-      id: 1,
-      title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-      userId: 1,
-      id: 2,
-      title: "qui est esse",
-      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-    }
-  ]);
+  const [data, setData] = useState<PostModel[]>([]);
+  const [error, setError] = useState<any>();
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
-  return (
-    <div className="App">
-      {data.map((item, index) => <Post key={index} post={item}/>)}
-    </div>
-  );
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setData(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className="App">
+        {data.map((item, index) => <Post key={index} post={item}/>)}
+      </div>
+    );
+  }
 }
 
 export default App;
